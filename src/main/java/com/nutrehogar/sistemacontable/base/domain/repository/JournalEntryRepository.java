d7f4bd810ce483540576a252ce391ed067d4ed8f
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-11-22 00:11:07 -0500

    feat: suggest new document id number

--- src/main/java/com/nutrehogar/sistemacontable/base/domain/repository/JournalEntryRepository.java
+++ src/main/java/com/nutrehogar/sistemacontable/base/domain/repository/JournalEntryRepository.java
@@ -2,10 +2,13 @@ package com.nutrehogar.sistemacontable.base.domain.repository;
 
 import com.nutrehogar.sistemacontable.domain.model.JournalEntry;
 import com.nutrehogar.sistemacontable.domain.model.JournalEntryPK;
+import com.nutrehogar.sistemacontable.domain.type.DocumentType;
 import com.nutrehogar.sistemacontable.exception.RepositoryException;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface JournalEntryRepository extends CRUDRepository<JournalEntry, JournalEntryPK> {
     List<JournalEntry> findAllByDateRange(LocalDate startDate, LocalDate endDate) throws RepositoryException;
+
+    JournalEntry findLast(DocumentType type) throws RepositoryException;
 }
